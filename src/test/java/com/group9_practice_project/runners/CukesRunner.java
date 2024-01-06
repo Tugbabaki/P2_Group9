f745f16ca002331a92ebc3dfa41ef4178e2171a1
Tugbabaki <tugbabakiki@gmail.com>
2024-01-06 16:42:53 -0400

    Initial commit

---
+++ src/test/java/com/group9_practice_project/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.group9_practice_project.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/group9_practice_project/step_definitions",
+        dryRun = true,
+        tags = "@wip",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
---
+++ src/test/java/com/group9_practice_project/step_definitions/US07_StepDefs_TB.java
@@ -0,0 +1,4 @@
+package com.group9_practice_project.step_definitions;
+
+public class US07_StepDefs_TB {
+}
