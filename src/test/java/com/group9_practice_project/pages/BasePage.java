5f1cbcf196161a957ffc5760471cfba7dc4b0c22
svetko <ssadmitrovic@gmail.com>
2024-01-10 20:56:37 -0500

    US04 feature file steps updated

--- src/test/java/com/group9_practice_project/pages/BasePage.java
+++ src/test/java/com/group9_practice_project/pages/BasePage.java
@@ -1,9 +1,18 @@
 package com.group9_practice_project.pages;
 
+import com.group9_practice_project.utilities.BrowserUtils;
 import com.group9_practice_project.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public abstract class BasePage {
 
@@ -37,10 +46,6 @@ public abstract class BasePage {
 
     @FindBy(linkText = "My User")
     public WebElement myUser;
-
-    public BasePage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
 
 
     /**
