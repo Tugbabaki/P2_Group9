c1db68d71fe7c760a0fd80aa8ed9b6807f2521f2
svetko <ssadmitrovic@gmail.com>
2024-01-10 20:29:19 -0500

    US04 feature file steps updated

--- src/test/java/com/group9_practice_project/step_definitions/US04_StepDefs_SD.java
+++ src/test/java/com/group9_practice_project/step_definitions/US04_StepDefs_SD.java
@@ -57,8 +57,6 @@ public class US04_StepDefs_SD {
         Assert.assertEquals(expectedURL, actualURL);
     }
 
-
-
     @Then("user should see the required message")
     public void userShouldSeeTheRequiredMessage() {
 
@@ -67,8 +65,6 @@ public class US04_StepDefs_SD {
 
         Assert.assertEquals(expectedMessage, actualMessage);
     }
-
-
 
 
 }
